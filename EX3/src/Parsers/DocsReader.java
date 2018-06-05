091586ce2b39791655dd61aef752c04698b05a43
yanivid3 <yanivid3@walla.com>
2018-06-05 15:18:01 +0300

    Read all text

--- EX3/src/Parsers/DocsReader.java
+++ EX3/src/Parsers/DocsReader.java
@@ -1,11 +1,19 @@
 package Parsers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import org.apache.lucene.index.IndexWriter;
 
 public class DocsReader {
 
-	public void LoadAndIndexDocs(IndexWriter indexWriter, String docsFile, Boolean isImprovedAlgo) {
-		// TODO Auto-generated method stub
-		
+	public void LoadAndIndexDocs(IndexWriter indexWriter, String docsFile, Boolean isImprovedAlgo) throws IOException {
+		Path  docsFilePath = Paths.get(docsFile);
+	    byte[] encoded = Files.readAllBytes(docsFilePath);
+	    String docs =  new String(encoded);
+	    		
 	}
 }
